6d8459d9e40b8f3c6e953495ee8d56134113d39c
juandiegomunoz@gmail.com <juandiego@codigosolido.com>
2020-02-26 23:11:42 +0000

    Create Application project

--- SalesReceiptApp/Program.cs
+++ SalesReceiptApp/Program.cs
@@ -2,10 +2,13 @@
 //     Copyright (c) Lastminute. All rights reserved.
 // </copyright>
 
-namespace Lastminute.SalesReceipt
+namespace Lastminute.SalesReceiptApp
 {
     using System;
+    using System.Collections.Generic;
     using System.IO;
+    using Microsoft.Extensions.Configuration;
+    using Lastminute.SalesReceipt;
 
     /// <summary>
     /// Execution point of the project.
@@ -21,28 +24,22 @@ namespace Lastminute.SalesReceipt
             // Start
             Console.WriteLine("Starting Sales Receipt Program");
 
+            // Load config
+            IConfiguration config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+
             // Set Taxes
-            ItemTaxable.ApplyTax(new Tax
+            foreach (Tax tax in config.GetSection("Taxes").Get<List<Tax>>())
             {
-                Name = "Base Tax",
-                Exclude = new string[] {
-                    "CHOCOLATE",
-                    "PILLS",
-                    "BOOK" },
-                Rate = 10
-            });
-            ItemTaxable.ApplyTax(new Tax
-            {
-                Name = "Import Tax",
-                Include = new string[] { "IMPORTED" },
-                Rate = 5
-            });
+                ItemTaxable.ApplyTax(tax);
+            }
 
             // Read files in the input folder
             foreach (string file in Directory.GetFiles("data"))
             {
+                Console.WriteLine(Environment.NewLine + new string('=', 20));
                 Console.WriteLine($"FILE : {file}");
-                Console.WriteLine("INPUT");
+                Console.WriteLine(Environment.NewLine + "INPUT");
+                Console.WriteLine(new string('-', 5));
                 ItemList list = new ItemList();
                 foreach (string line in File.ReadLines(file))
                 {
@@ -52,7 +49,8 @@ namespace Lastminute.SalesReceipt
                     Console.WriteLine(line);
                 }
 
-                Console.WriteLine("OUTPUT");
+                Console.WriteLine(Environment.NewLine + "OUTPUT");
+                Console.WriteLine(new string('-', 6));
                 Console.WriteLine(list.ToString());
             }
 
